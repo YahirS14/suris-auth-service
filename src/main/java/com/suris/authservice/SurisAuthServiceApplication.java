b1a8a8c712fad9ea94e757e97cc2c203b8b4c736
Yahir <yahiratzin@gmail.com>
2025-07-23 22:12:15 -0600

    Initialize Project

---
+++ src/main/java/com/suris/authservice/SurisAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.suris.authservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurisAuthServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SurisAuthServiceApplication.class, args);
+    }
+
+}
